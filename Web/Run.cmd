8de33b8302222309c3225f10e59feadab12963ec
M Aswin Kishore <mak26.work@gmail.com>
2020-10-26 21:53:08 +0530

    Update Run.cmd

--- Web/Run.cmd
+++ Web/Run.cmd
@@ -1,48 +1,34 @@
 @echo off
 mode con: cols=82 lines=32
 color 0F
-title MAK VS CODE JAVA INSTALLER
+setlocal enabledelayedexpansion
+title MAK VS CODE JAVA WEB INSTALLER
 echo ----------------------------------------------------------------------------------
-echo                           VS CODE AND JAVA INSTALLER
+echo                         VS CODE AND JAVA WEB INSTALLER
 echo                                  Author: MAK 
 echo ----------------------------------------------------------------------------------
 echo.
-
 set locationvs="C:\Users\%USERNAME%\AppData\Local\Programs\Microsoft VS Code\bin\code"
-echo Just Press Yes.. Next Next Next.. Install.. Finish/Close When Prompted
-echo.
 
 @rem main function
 if exist "C:\Users\%USERNAME%\AppData\Local\Programs\Microsoft VS Code\bin" (
-	"%cd%\python\python.exe" "download-jdk.py"
-	if %ERRORLEVEL% == 0 ( 
-	color 0C
-	echo Files not found
-	echo ----------------------------------------------------------------------------------
-	echo Press Enter To Exit 
-	pause>NUl
+	"%cd%\python\python.exe" "download.py" "java" 
+	if !errorlevel! EQU 1 ( 
+	call :filenotfound
 	EXIT 
     )
-
+	cd /d data
+	call :startmessage
 	call :onlyjava	
-	color 0E
-	echo.
-	echo Copy lines from the sample.txt to settings.json
-	call %locationvs% -n -g sample.json "C:\Users\%USERNAME%\AppData\Roaming\Code\User\settings.json" 
-	echo Press Enter Once Done Copying The Lines
-	pause>NUL
-	taskkill /f /im Code.exe>nul 2>&1
 	
 ) else (
-	"%cd%\python\python.exe" "download-VSjdk.py"
-	if %ERRORLEVEL% == 0 ( 
-	color 0C
-	echo Files not found
-	echo ----------------------------------------------------------------------------------
-	echo Press Enter To Exit 
-	pause>NUl
+	"%cd%\python\python.exe" "download.py" "vs" 
+	if !errorlevel! EQU 1 ( 
+	call :filenotfound
 	EXIT 
     )
+	cd /d data
+	call :startmessage
 	call :firstinstallation	
 )
 
@@ -54,27 +40,45 @@ pause>NUl
 EXIT 
 
 :onlyjava
-	cd /d data
-	echo Installing Java.. 
-	start /W JavaCodingPack.exe
+	echo.
+	echo Installing Adobe Open JDK 11..
+	start /W JDK.msi
 	taskkill /f /im Code.exe>nul 2>&1
-	echo Installed Extensions
+	echo Installing Java and Extension Pack..
+	start /W JavaCodingPack.exe
 	exit /B 0
 
 :firstinstallation
-	cd /d data
+	echo.
 	echo Installing VS CODE..
 	start /W VS.exe
 	taskkill /f /im Code.exe>nul 2>&1
-	xcopy "%cd%/Code" %appdata% /e /y /q /i>NUL
-	echo Configurations Set
 	call :onlyjava
 	exit /B 0
 
 :test
 	echo.
 	echo Test VS Code By Running The test.java Program
+	echo Press Enter To open test.java in VS Code for testing
+	pause>NUL
 	call %locationvs% -n -g Test.java
 	echo.
 	exit /B 0
 	
+:startmessage
+	echo.
+	echo Just Press Yes/Run .. Next Next Next.. Install.. Finish/Close When Prompted
+	color 0E
+	echo.
+	echo Press Enter To Start Installing..
+	pause>NUL
+	color 0F
+	exit /B 0
+
+:filenotfound
+	color 0C
+	echo Files not found
+	echo ----------------------------------------------------------------------------------
+	echo Press Enter To Exit 
+	pause>NUl
+	exit /B 0
